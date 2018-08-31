57ad2b5c824fa42f348f6995b01718a69f7efc5a
natalia <natalia.gulko@yahoo.com>
2018-08-31 13:16:59 -0700

    updated repo name2

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -4,10 +4,10 @@ DOCKER_BUILD_TAG=$1
 echo "Docker build tag: " $DOCKER_BUILD_TAG
 if [[ "$DOCKER_BUILD_TAG" = "latest" ]]; then
         echo "tag - master"
-	docker build --build-arg BUILD_TAG="master" --no-cache=true --tag parity/parity:$DOCKER_BUILD_TAG .
+	docker build --build-arg BUILD_TAG="master" --no-cache=true --tag poanetwork/parity:$DOCKER_BUILD_TAG .
 else
          echo "not master"
-	docker build --build-arg BUILD_TAG=$DOCKER_BUILD_TAG --no-cache=true --tag parity/parity:$DOCKER_BUILD_TAG .
+	docker build --build-arg BUILD_TAG=$DOCKER_BUILD_TAG --no-cache=true --tag poanetwork/parity:$DOCKER_BUILD_TAG .
 fi
 echo "docker run"
 docker run -it poanetwork/parity:$DOCKER_BUILD_TAG -v
