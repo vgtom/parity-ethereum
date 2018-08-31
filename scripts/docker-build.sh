5b0c2977ae23324fdd6371adf93f2ac7eb824eab
natalia <natalia.gulko@yahoo.com>
2018-08-31 13:13:00 -0700

    Added logs, changed docker repo name, enable some builds for server 3, set docker-build for shell executor

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -3,9 +3,13 @@ cd docker/hub
 DOCKER_BUILD_TAG=$1
 echo "Docker build tag: " $DOCKER_BUILD_TAG
 if [[ "$DOCKER_BUILD_TAG" = "latest" ]]; then
+        echo "tag - master"
 	docker build --build-arg BUILD_TAG="master" --no-cache=true --tag parity/parity:$DOCKER_BUILD_TAG .
 else
+         echo "not master"
 	docker build --build-arg BUILD_TAG=$DOCKER_BUILD_TAG --no-cache=true --tag parity/parity:$DOCKER_BUILD_TAG .
 fi
-docker run -it parity/parity:$DOCKER_BUILD_TAG -v
-docker push parity/parity:$DOCKER_BUILD_TAG
+echo "docker run"
+docker run -it poanetwork/parity:$DOCKER_BUILD_TAG -v
+echo "docker push"
+docker push poanetwork/parity:$DOCKER_BUILD_TAG
