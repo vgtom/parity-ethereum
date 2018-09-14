fabaca0e2778bea570fa956e58ae5280bfd9f5a9
c0gent <nsan1129@gmail.com>
2018-09-14 06:56:24 -0700

    Experiment with blocks.

--- ethcore/src/client/hbbft_adapter.rs
+++ ethcore/src/client/hbbft_adapter.rs
@@ -1,23 +1,178 @@
-#![allow(dead_code)]
+#![allow(dead_code, unused_imports, unused_variables)]
 
+use std::thread;
 use futures::sync::mpsc::Receiver;
 use client::Client;
 
 pub struct HbbftAdapter {
 	client: Client,
 	block_rx: Receiver<u8>,
+	th: thread::JoinHandle<()>,
 }
 
 impl HbbftAdapter {
+	pub fn new(client: Client, block_rx: Receiver<u8>,) -> HbbftAdapter {
+
+		let th = thread::Builder::new().name("hbbft-adapter".into()).spawn(|| {
+
+		}).unwrap();
+
+		HbbftAdapter {
+			client,
+			block_rx,
+			th,
+		}
+	}
 
 }
 
 
 #[cfg(test)]
 mod tests {
+	use client::{TestBlockChainClient, EachBlockWith, BlockId, BlockChainClient,
+		Nonce, Balance, ChainInfo, BlockInfo, CallContract, TransactionInfo,
+		RegistryInfo, ReopenBlock, PrepareOpenBlock, ScheduleInfo, ImportSealedBlock,
+		BroadcastProposalBlock, ImportBlock, StateOrBlock, StateInfo, StateClient, Call,
+		AccountData, BlockChain as BlockChainTrait, BlockProducer, SealedBlockImporter,
+		ClientIoMessage,
+	};
+
+	use verification::queue::kind::blocks::{Unverified};
+	use rlp::{Rlp, RlpStream, DecoderError};
+	use block::{OpenBlock, SealedBlock, ClosedBlock};
+	use header::Header;
+
+
 
 	#[test]
 	fn add_transaction() {
+		let client = TestBlockChainClient::new();
 
+		let bad_block = Unverified {
+			header: Header::default(),
+			transactions: vec![],
+			uncles: vec![],
+			bytes: vec![1, 2, 3],
+		};
+
+		client.import_block(bad_block).unwrap();
+	}
+}
+
+
+#[cfg(feature = "ref_000")]
+mod ref_000 {
+
+	// ethcore/transaction/src/transaction.rs
+	//
+	/// A set of information describing an externally-originating message call
+	/// or contract creation operation.
+	#[derive(Default, Debug, Clone, PartialEq, Eq)]
+	pub struct Transaction {
+		/// Nonce.
+		pub nonce: U256,
+		/// Gas price.
+		pub gas_price: U256,
+		/// Gas paid up front for transaction execution.
+		pub gas: U256,
+		/// Action, can be either call or contract create.
+		pub action: Action,
+		/// Transfered value.
+		pub value: U256,
+		/// Transaction data.
+		pub data: Bytes,
+	}
+
+	// ethcore/transaction/src/transaction.rs
+	//
+	/// Signed transaction information without verified signature.
+	#[derive(Debug, Clone, Eq, PartialEq)]
+	pub struct UnverifiedTransaction {
+		/// Plain Transaction.
+		unsigned: Transaction,
+		/// The V field of the signature; the LS bit described which half of the curve our point falls
+		/// in. The MS bits describe which chain this transaction is for. If 27/28, its for all chains.
+		v: u64,
+		/// The R field of the signature; helps describe the point on the curve.
+		r: U256,
+		/// The S field of the signature; helps describe the point on the curve.
+		s: U256,
+		/// Hash of the transaction
+		hash: H256,
+	}
+
+	// ethcore/src/header.rs
+	//
+	/// A block header.
+	///
+	/// Reflects the specific RLP fields of a block in the chain with additional room for the seal
+	/// which is non-specific.
+	///
+	/// Doesn't do all that much on its own.
+	#[derive(Debug, Clone, Eq)]
+	pub struct Header {
+		/// Parent hash.
+		parent_hash: H256,
+		/// Block timestamp.
+		timestamp: u64,
+		/// Block number.
+		number: BlockNumber,
+		/// Block author.
+		author: Address,
+
+		/// Transactions root.
+		transactions_root: H256,
+		/// Block uncles hash.
+		uncles_hash: H256,
+		/// Block extra data.
+		extra_data: Bytes,
+
+		/// State root.
+		state_root: H256,
+		/// Block receipts root.
+		receipts_root: H256,
+		/// Block bloom.
+		log_bloom: Bloom,
+		/// Gas used for contracts execution.
+		gas_used: U256,
+		/// Block gas limit.
+		gas_limit: U256,
+
+		/// Block difficulty.
+		difficulty: U256,
+		/// Vector of post-RLP-encoded fields.
+		seal: Vec<Bytes>,
+
+		/// Memoized hash of that header and the seal.
+		hash: Option<H256>,
+	}
+
+	// ethcore/src/verification/queue/kind.rs
+	//
+	/// An unverified block.
+	#[derive(PartialEq, Debug)]
+	pub struct Unverified {
+		/// Unverified block header.
+		pub header: Header,
+		/// Unverified block transactions.
+		pub transactions: Vec<UnverifiedTransaction>,
+		/// Unverified block uncles.
+		pub uncles: Vec<Header>,
+		/// Raw block bytes.
+		pub bytes: Bytes,
+	}
+
+	// ethcore/src/verification/verification.rs
+	//
+	/// Preprocessed block data gathered in `verify_block_unordered` call
+	pub struct PreverifiedBlock {
+		/// Populated block header
+		pub header: Header,
+		/// Populated block transactions
+		pub transactions: Vec<SignedTransaction>,
+		/// Populated block uncles
+		pub uncles: Vec<Header>,
+		/// Block bytes
+		pub bytes: Bytes,
 	}
 }
