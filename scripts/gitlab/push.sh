0e4a0b3396739a4c00528bb67a1b8fd02565ac3a
natalia <natalia.gulko@yahoo.com>
2018-09-25 16:18:01 -0700

    Call updater_push_release

--- scripts/gitlab/push.sh
+++ scripts/gitlab/push.sh
@@ -9,6 +9,9 @@ updater_push_release () {
   echo "Pushing release to Sokol"
   ./safe_curl.sh $DATA "http://45.32.70.198:1339/push-release/$CI_BUILD_REF_NAME/$CI_BUILD_REF"
 }
+
+updater_push_release
+
 echo "__________Set ENVIROMENT__________"
 DESCRIPTION="$(cat CHANGELOG.md)"
 RELEASE_TABLE="$(cat scripts/gitlab/templates/release-table.md)"
