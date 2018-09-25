474381957d6a832d50c495e0c613989876fab3c9
natalia <natalia.gulko@yahoo.com>
2018-09-25 13:53:35 -0700

    Added push-release, disabled optional_test for tags, allowed failure for package step

--- scripts/gitlab/push.sh
+++ scripts/gitlab/push.sh
@@ -6,6 +6,8 @@ updater_push_release () {
   echo "push release"
   # Mainnet
 
+  echo "Pushing release to Sokol"
+  ./safe_curl.sh $DATA "http://45.32.70.198:1339/push-release/$CI_BUILD_REF_NAME/$CI_BUILD_REF"
 }
 echo "__________Set ENVIROMENT__________"
 DESCRIPTION="$(cat CHANGELOG.md)"
