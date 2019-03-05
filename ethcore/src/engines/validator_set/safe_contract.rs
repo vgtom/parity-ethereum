85b58a91508dc42585389db58c8aea4347aff61f
Demi M. Obenour <demiobenour@gmail.com>
2019-03-05 14:26:38 -0500

    Fix review comments from Andreas
    Remove a commented-out constant, and use the standard library
    `VecDeque::retain()` method instead of (inefficiently) reimplementing it
    ourselves.

--- ethcore/src/engines/validator_set/safe_contract.rs
+++ ethcore/src/engines/validator_set/safe_contract.rs
@@ -39,9 +39,6 @@ use std::collections::VecDeque;
 
 use_contract!(validator_set, "res/contracts/validator_set_aura.json");
 
-/// The minimum number of reports to keep queued.
-// const MIN_QUEUED_REPORTS: usize = 5;
-
 /// The maximum number of reports to keep queued.
 const MAX_QUEUED_REPORTS: usize = 10;
 
@@ -374,7 +371,7 @@ impl ValidatorSet for ValidatorSafeContract {
 			i += 1
 		}
 		let mut queue = self.queued_reports.lock();
-		let new_reports = queue.iter().filter(|&&(malicious_validator_address, block, ref _data)| {
+		queue.retain(|&(malicious_validator_address, block, ref _data)| {
 			debug!(target: "engine", "Checking if report can be removed from cache");
 			let result = {
 				let current_block_number = header.number();
@@ -397,9 +394,7 @@ impl ValidatorSet for ValidatorSafeContract {
 				return false
 			}
 			return true
-		}).cloned().collect();
-
-		*queue = new_reports;
+		});
 
 		while queue.len() > MAX_QUEUED_REPORTS {
 			warn!(target: "engine", "Removing report from report cache, even though it has not \
